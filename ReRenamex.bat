e6032b587b92ed5688669df6204c6195fa53749f
iffizier <72680130+iffizier@users.noreply.github.com>
2021-03-20 11:37:38 -0700

    Create ReRenamex.bat

---
+++ ReRenamex.bat
@@ -0,0 +1,17 @@
+rename "*.3gpx" *.3gp
+rename "*.avix" *.avi
+rename "*.flvx" *.flv
+rename "*.mkvx" *.mkv
+rename "*.movx" *.mov
+rename "*.mpgx" *.mpg
+rename "*.mpegx" *.mpeg
+rename "*.mp4x" *.mp4
+rename "*.rmvbx" *.rmvb
+rename "*.tsx" *.ts
+rename "*.webmx" *.webm
+rename "*.wmvx" *.wmv
+rename "*.gifx" *.gif
+rename "*.jpgx" *.jpg
+rename "*.jpegx" *.jpeg
+rename "*.pngx" *.png
+rename "*.webpx" *.webp
